9551548cc60bdeb79ecbe21d01bdafb8421e708c
anderswag <anderson.hung@jumbleberry.com>
2019-04-10 15:28:22 -0400

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" autofocus=true v-on:keyup.enter="addItem"  v-model="task" placeholder="lets rock and roll"/>
+    <button v-on:click="addItem">Add</button>
+    <ul>
+      <li 
+        v-for="task in allTasks" 
+        v-on:click="checkItem(task)" 
+        v-on:dblclick="deleteItem(task)"
+      >
+        <h1 v-bind:class="{complete: task.isComplete}">{{task.name}}</h1>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import { mapActions } from 'vuex'
 
 export default {
   name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      task: ''
+    }
+  },
+  methods: {
+    addItem: function() {
+      this.$store.dispatch('addItem', this.task)
+      this.task = ''
+    },
+    deleteItem: function(task) {
+      let itemId = task.id
+      this.$store.dispatch('deleteItem', itemId)
+    },
+    checkItem: function(task) {
+      let itemId = task.id
+      this.$store.dispatch('checkItem', itemId)
+    },
+  },
+  computed: {
+    allTasks() {
+      return this.$store.state.items
+    }
   }
 };
 </script>
@@ -24,5 +59,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+  list-style-type: none
+}
+.complete { 
+  color: grey;
+  text-decoration: line-through;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -3,8 +3,37 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 10000)
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    items: [],
+  },
+  mutations: {
+    addItem (state, item) {
+      if (!item) { return }
+      state.items.push({id: getRandomNumber(),name: item, isComplete: false})
+    },
+    deleteItem (state, itemId) {
+      const filteredItems = state.items.filter((item) => item.id != itemId)
+      state.items = filteredItems
+    },
+    checkItem (state, itemId) {
+      const itemIndex = state.items.findIndex((item) => item.id === itemId)
+      state.items[itemIndex].isComplete = !state.items[itemIndex].isComplete
+    }
+  },
+  actions: {
+    addItem (context, item) {
+      context.commit('addItem', item)
+    },
+    deleteItem (context, itemId) {
+      context.commit('deleteItem', itemId)
+    },
+    checkItem (context, itemId) {
+      context.commit('checkItem', itemId)
+    }
+  }
 });
